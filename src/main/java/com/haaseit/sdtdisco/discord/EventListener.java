63c0ce849d399d739691feeb197a41b785296451
Marcus Haase <mail@marcus.haase.name>
2017-08-03 20:45:06 +0200

    moved channel object setup to readyEvent

--- src/main/java/com/haaseit/sdtdisco/discord/EventListener.java
+++ src/main/java/com/haaseit/sdtdisco/discord/EventListener.java
@@ -20,20 +20,24 @@ public class EventListener {
 
     @EventSubscriber
     public void onReadyEvent(ReadyEvent event) {
+        messageHandler.setChannel(channel);
+        if (adminchannel != null) {
+            messageHandler.setAdminChannel(adminchannel);
+        }
         System.out.println("Ready.");
     }
 
     @EventSubscriber
     public void onMessageReceivedEvent(MessageReceivedEvent event) {
         if (event.getChannel().getStringID().equals(channel)) {
-            System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Channel: " + event.getAuthor().getName() + "> " + event.getMessage());
+            //System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Channel: " + event.getAuthor().getName() + "> " + event.getMessage().getContent());
             // todo: regular channel should send everything except whitelisted commands (eg. /time) to server by "Say".
-            messageHandler.handleMessageFromChannel(event.getAuthor().getName() + ": " + event.getMessage());
+            messageHandler.handleMessageFromChannel(event.getAuthor().getName() + ": " + event.getMessage().getContent());
         } else if (adminchannel != null && event.getChannel().getStringID().equals(adminchannel)) {
-            System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Admin: " + event.getAuthor().getName() + "> " + event.getMessage());
-            // todo: admin channel posts directly to server console, so you can execute commands
+            //System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Admin: " + event.getAuthor().getName() + "> " + event.getMessage().getContent());
+            messageHandler.handleMessageFromAdminChannel(event.getMessage().getContent());
         } else if (event.getChannel().isPrivate()) {
-            System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Direct: " + event.getAuthor().getName() + "> " + event.getMessage());
+            System.out.println(Helper.getCurrentLocalDateTimeFormatted() + " Direct: " + event.getAuthor().getName() + "> " + event.getMessage().getContent());
             // todo: direct messages should only listen to whitelisted commands
         }
 
